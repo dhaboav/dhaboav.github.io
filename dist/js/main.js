b205912d9c272736e4b34c79cd502c063151eb9b
dhaboav <100108392+Dhaboav@users.noreply.github.com>
2025-11-30 13:42:03 +0700

    feat: Added BackToTop component
    - Changed the language attribute in index.html from "id" to "en".
    - Removed the back-to-top link from index.html.
    - Created a new BackToTop component in BackToTop.js to handle the back-to-top functionality.
    - Cleaned up unused CSS styles related to the navbar and hamburger menu in input.css.

--- dist/js/main.js
+++ dist/js/main.js
@@ -5,6 +5,9 @@ import Project from '../../src/components/Project.js'
 import Experience from '../../src/components/Experience.js'
 import Contact from '../../src/components/Contact.js'
 import Footer from '../../src/components/Footer.js'
+import BackToTop from '../../src/components/BackToTop.js'
+
+document.body.appendChild(BackToTop())
 
 const navbarContainer = document.getElementById('navbar-container')
 navbarContainer.appendChild(Navbar())
@@ -26,3 +29,23 @@ contactContainer.appendChild(Contact())
 
 const footerContainer = document.getElementById('footer-container')
 footerContainer.appendChild(Footer())
+
+// Scroll behavior for navbar + back to top
+window.addEventListener('scroll', () => {
+    const header = document.querySelector('header')
+    const toTop = document.querySelector('#to-top')
+
+    if (!header || !toTop) return
+
+    const fixedNav = header.offsetTop
+
+    if (window.scrollY > fixedNav) {
+        header.classList.add('navbar-fixed')
+        toTop.classList.remove('hidden')
+        toTop.classList.add('flex')
+    } else {
+        header.classList.remove('navbar-fixed')
+        toTop.classList.add('hidden')
+        toTop.classList.remove('flex')
+    }
+})
