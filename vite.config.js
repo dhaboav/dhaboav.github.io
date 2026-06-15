fe6a5cece60784b48a2ddbeafa7d10b90ed3fbf5
dhaboav <100108392+dhaboav@users.noreply.github.com>
2026-06-15 13:43:57 +0700

    build: Add gh-pages depedency

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
 
 export default defineConfig({
+  base: '/dhaboav.github.io/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
